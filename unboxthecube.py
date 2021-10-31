6da5eb540055f3d7c24d2759c250b505964f3acb
Evan He <e7he@uwaterloo.ca>
2021-10-31 14:38:05 -0400

    Added unboxthecube class

--- unboxthecube.py
+++ unboxthecube.py
@@ -1,15 +1,81 @@
+from unboxtheface import Face
+
+CLOCK_WISE = 0
+COUNTER_CLOCKWISE = 1
+
+U_F = 0 #up
+L_F = 1 #left
+F_F = 2 #front
+R_F = 3 #right
+B_F = 4 #bottom
+D_F = 5 #down
 
 class Cube:
+
+    '''
+
+    
+
+
+    '''
 
     faces = []
 
     def __init__(self):
-        self.faces = [Face(), Face(), Face(), Face(), Face(), Face()]
+        self.faces = [Face(U_F, B_F, F_F, L_F, R_F), Face(L_F, U_F, R_F, B_F, F_F), Face(F_F, U_F, D_F, L_F, R_F), Face(R_F, U_F, D_F, F_F, B_F), Face(B_F, U_F, D_F, R_F, L_F), Face(D_F, F_F, B_F, L_F, R_F)]
+
+    
+    def rotate(self, index, direction):
+
+        if direction == CLOCK_WISE:
+            
+            #rotate the face
+            faces[index].rotate_clock_wise(direction);
+
+            #rotate the adjacent small cube
+            
+            
+
+        
+    
+        
+    def clear(self):
+
+        #clear all faces
+        for face in self.faces:
+            face.clear()
 
 
 
+
+    '''DEBUG FUNCTIONS'''
+
+    def check_integrity(self)-> bool:
+
+    #cube must have 6 faces
+        if len(self.faces) != 6:
+            return False
+        
+     #check each face's integrity
+        for face in faces:
+            if not face.check_integrity():
+                return False
+
+        return True;
+
+    def print_cube(self):
+
+        for face in self.faces:
+            print(face.curr)
+            face.print_face();
+            print('\n')
+    
+    
         
 
 
 if __name__ == "__main__":
     print("test\n")
+
+    cube = Cube()
+    cube.print_cube()
--- unboxtheface.py
+++ unboxtheface.py
@@ -1,11 +1,18 @@
 class Face:
     matrix = []
+    curr = 0
+    top = 0
+    down = 0
+    left = 0
+    right = 0
 
-    def __init__(self):
+    def __init__(self, new_curr, new_top, new_down, new_left, new_right):
         self.matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-    def __init__(self, new_matrix):
-        self.matrix = new_matrix
+        self.curr = new_curr
+        self.top = new_top
+        self.down = new_down
+        self.left = new_left
+        self.right = new_right
 
     def clear(self):
         self.matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -16,23 +23,6 @@ class Face:
             for j in range(3):
                 if (self.matrix[i][j] != first_colour):
                     return False
-        return True
-
-    def check_integrity(self):
-        #check to see if the lenghts of ecah part of the list are correct
-        if (self.matrix.length != 3) ||
-            (self.matrix[0].length != 3) ||
-            (self.matrix[1].length != 3) ||
-            (self.matrix[2].length != 3):
-                return False
-
-        #check to make sure all values in the list are 0-5 or a color Name
-        allowed_vals = [0,1,2,3,4,5,'R','G','B','O','W','Y','r','g','b','o','w','y']
-        for i in range(3):
-            for j in range(3):
-                if self.matrix[i][j] not in allowed_vals:
-                    return False
-
         return True
 
     def transpose(self):
@@ -51,9 +41,7 @@ class Face:
 
     def swap_cols(self):
         for row in range(3):
-            swap_var = self.matrix[row][0]
-            self.matrix[row][0] = self.matrix[row][2]
-            self.matrix[row][2] = swap_var
+            self.matrix[row][0], self.matrix[row][2] = self.matrix[row][2], self.matrix[row][0]
 
     #just rotates the 8 squares on this face. Not the neighbouring ones
     def rotate_face_ccw(self):
@@ -64,4 +52,31 @@ class Face:
     def rotate_face_cw(self):
         self.matrix = self.transpose()
         self.swap_cols()
+
+
+    #DEBUG
+    def check_integrity(self) -> bool:
+        #check to see if the lenghts of ecah part of the list are correct
+        if len(self.matrix) != 3:
+            return False
+
+        for m in self.matrix:
+            if len(m) != 3:
+                return False
+
+        #check to make sure all values in the list are 0-5 or a color Name
+        allowed_vals = [0,1,2,3,4,5,'R','G','B','O','W','Y','r','g','b','o','w','y']
+        for i in range(3):
+            for j in range(3):
+                if self.matrix[i][j] not in allowed_vals:
+                    return False
+
+        return True
+
+    def print_face(self):
+
+        for row in self.matrix:
+            print(row)
+
+        print()
         
